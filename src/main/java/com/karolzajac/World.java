ef1c226483297d1b40e22ee691840dbf9bcf209e
KarolZajac <73061445+KarolZajac@users.noreply.github.com>
2020-12-21 22:29:33 +0100

    Update World.java

--- src/main/java/com/karolzajac/World.java
+++ src/main/java/com/karolzajac/World.java
@@ -1,24 +1,7 @@
 package com.karolzajac;
 
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ScheduledExecutorService;
-import java.util.concurrent.TimeUnit;
-
 import com.karolzajac.world.Jungle;
-import com.karolzajac.world.SimulationEngine;
-import com.karolzajac.world.map.RectangularMap;
-import javafx.geometry.Pos;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.GridPane;
-import javafx.stage.Stage;
 
 public class World {
     public static void main(String[] args) {
